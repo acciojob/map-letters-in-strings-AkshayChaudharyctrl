1230785c97f9306e516a537ec91145f0818630cd
Akshay Chaudhary <124782758+AkshayChaudharyctrl@users.noreply.github.com>
2023-06-19 10:25:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+function mapLetters(word) {
+  const letters = {};
+
+  for (let i = 0; i < word.length; i++) {
+    const letter = word[i];
+
+    if (!letters.hasOwnProperty(letter)) {
+      letters[letter] = [];
+    }
+
+    letters[letter].push(i);
+  }
+
+  return letters;
+}
